7645643b4dd9f58b5be3690c8075532d6389e142
gereleth <daria.voznyak@gmail.com>
2022-06-28 00:09:08 +0300

    First dirty version of flow vis

--- src/lib/puzzle/Puzzle.svelte
+++ src/lib/puzzle/Puzzle.svelte
@@ -2,6 +2,7 @@
     import { HexaGrid, YSTEP } from "$lib/hexagrid";
     import Tile from '$lib/puzzle/Tile.svelte';
     import { onMount, createEventDispatcher } from 'svelte';
+    import {randomColor} from 'randomcolor';
 
     /**
      * @param {Number[]} tiles
@@ -14,6 +15,11 @@
     ]
     let solved = false
 
+    let tileComponents = new Map([])
+    let components = new Map([
+       [ -1, {id: -1, color: 'white', tiles: new Set([])}]
+    ])
+
     const dispatch = createEventDispatcher()
 
     let grid = new HexaGrid(width, height)
@@ -23,8 +29,112 @@
     let innerWidth = 500
     let innerHeight = 500
     let startCheckAtIndex = 0
+    let initialized = false
+    let componentCounter = 1
 
     $: pxPerCell = resize(innerWidth, innerHeight)
+
+    function getTileComponent(tileIndex) {
+        const componentIndex = tileComponents.get(tileIndex)
+        const component = components.get(componentIndex)
+        return component
+    }
+
+    function mergeComponents(fromIndex, toIndex) {
+        const fromComponent = getTileComponent(fromIndex)
+        const toComponent = getTileComponent(toIndex)
+        if (fromComponent === toComponent) {
+            if (initialized) {
+                // console.log('merge component to itself, its a loop')
+            }
+            return
+        }
+        const fromIsBigger = fromComponent.tiles.size >= toComponent.tiles.size
+        const constantComponent = fromIsBigger ? fromComponent : toComponent
+        const changedComponent = fromIsBigger ? toComponent : fromComponent
+        for (let changedTile of changedComponent.tiles) {
+            tileComponents.set(changedTile, constantComponent.id)
+            constantComponent.tiles.add(changedTile)
+        }
+        if (initialized) {
+            let newColor = constantComponent.color
+            if (newColor==='white') {
+                newColor = changedComponent.color
+            }
+            if (newColor==='white') {
+                newColor = randomColor()
+            }
+            constantComponent.color = newColor
+        }
+        components.delete(changedComponent.id)
+    }
+
+
+    function findConnectedTiles(fromIndex, toIndex) {
+        let toCheck = new Set([{fromIndex: fromIndex, tileIndex: toIndex}])
+        const myComponent = tileComponents.get(toIndex)
+        const checked = new Set([])
+        while (toCheck.size > 0) {
+            const newChecks = new Set([])
+            for (let{fromIndex, tileIndex} of toCheck) {
+                const neighbours = connections.get(tileIndex)
+                for (let neighbour of neighbours) {
+                    if (neighbour===-1) {
+                        // no neighbour
+                        continue
+                    }
+                    if (tileComponents.get(neighbour)!==myComponent) {
+                        // not from this component, will be handled during merge phase
+                        continue
+                    }
+                    const neighbourConnections = connections.get(neighbour)
+                    if (!neighbourConnections.has(tileIndex)) {
+                        // not mutual
+                        continue
+                    }
+                    if (neighbour === fromIndex) {
+                        // came from here
+                        continue
+                    }
+                    if (checked.has(neighbour)) {
+                        // it's a loop?
+                        continue
+                    }
+                    newChecks.add({fromIndex: tileIndex, tileIndex: neighbour})
+                }
+                checked.add(tileIndex)
+                toCheck = newChecks
+            }
+        }
+        return checked
+    }
+
+    function disconnectComponents(fromIndex, toIndex) {
+        const bigComponent = getTileComponent(fromIndex)
+        const fromTiles = findConnectedTiles(toIndex, fromIndex)
+        const toTiles = findConnectedTiles(fromIndex, toIndex)
+        if ([...fromTiles].some(tile=>toTiles.has(tile))) {
+            // it was a loop or maybe it still is
+            // console.log('not disconnecting because of other connection')
+            return
+        }
+        const fromIsBigger = fromTiles.size >= toTiles.size
+        const leaveTiles = fromIsBigger ? fromTiles : toTiles
+        const changeTiles = fromIsBigger ? toTiles : fromTiles
+        const newComponent = {
+                id: componentCounter,
+                color: randomColor(),
+                tiles: changeTiles,
+            }
+        components.set(newComponent.id, newComponent)
+        componentCounter += 1
+        
+        for (let tileIndex of changeTiles) {
+            tileComponents.set(tileIndex, newComponent.id)
+            bigComponent.tiles.delete(tileIndex)
+        }
+        // console.log('created new component', newComponent.id, 'with tiles', [...changeTiles])
+    }
 
     function resize(innerWidth, innerHeight) {
         const wpx = innerWidth / (width + 1.6)
@@ -34,79 +144,121 @@
 
     function handleConnections(event) {
         const {tileIndex, dirIn, dirOut} = event.detail
+        // console.log('==========================')
         // console.log(tileIndex, dirIn, dirOut)
-        if (connections.has(tileIndex)) {
-            const tileConnections = connections.get(tileIndex)
-            dirOut.forEach(direction => {
-                // console.log(grid.find_neighbour(tileIndex, direction))
-                tileConnections.delete(grid.find_neighbour(tileIndex, direction))
-            })
-            dirIn.forEach(direction => {
-                tileConnections.add(grid.find_neighbour(tileIndex, direction))
-            })
-        } else {
-            connections.set(tileIndex, new Set(dirIn.map(direction => {
-                return grid.find_neighbour(tileIndex, direction)
-            })))
-        }
-        if (event.detail.from==='user') {
+        const tileConnections  = connections.get(tileIndex)
+        dirOut.forEach(direction => {
+            const neighbour = grid.find_neighbour(tileIndex, direction)
+            if (neighbour===-1) {return}
+            tileConnections.delete(neighbour)
+            const neighbourComponent = components.get(tileComponents.get(neighbour))
+            const tileComponent = components.get(tileComponents.get(tileIndex))
+            if (tileComponent===neighbourComponent) {
+                // console.log('disconnecting components between tiles', tileIndex, neighbour)
+                disconnectComponents(tileIndex, neighbour)
+            }
+        })
+        dirIn.forEach(direction => {
+            const neighbour = grid.find_neighbour(tileIndex, direction)
+            if (neighbour===-1) {return}
+            tileConnections.add(neighbour)
+            const neighbourConnections = connections.get(neighbour)
+            if (!neighbourConnections.has(tileIndex)) {
+                return // non-mutual link shouldn't lead to merging
+            }
+            const tileComponent = components.get(tileComponents.get(tileIndex))
+            const neighbourComponent = components.get(tileComponents.get(neighbour))
+            if (tileComponent!==neighbourComponent) {
+                // console.log('merging components of tiles', tileIndex, neighbour)
+                mergeComponents(tileIndex, neighbour)
+            }
+        })
+        if (initialized) {
             solved = isSolved()
         }
+        components = components
     }
 
     function isSolved() {
-        // console.log('=================== Solved check ======================')
-        let toCheck = new Set([{fromIndex: -1, tileIndex: startCheckAtIndex}])
-        console.log('start at', startCheckAtIndex)
-        const checked = new Set([])
-        while (toCheck.size > 0) {
-            // console.log('toCheck = ', toCheck)
-            const newChecks = new Set([])
-            for (let{fromIndex, tileIndex} of toCheck) {
-                // console.log('checking tile', tileIndex, 'coming from', fromIndex)
-                const neighbours = connections.get(tileIndex)
-                // console.log('tile neighbours', neighbours)
-                for (let neighbour of neighbours) {
-                    // console.log('checking neighbour', neighbour)
-                    if (neighbour===-1) {
-                        // not solved if any tiles point outside
-                        // console.log('not solved for outside connection in tile', tileIndex)
-                        startCheckAtIndex = tileIndex
-                        return false
-                    }
-                    const neighbourConnections = connections.get(neighbour)
-                    // console.log('neighbour connections', neighbourConnections)
-                    if (!neighbourConnections.has(tileIndex)) {
-                        // not solved if a connection is not mutual
-                        // console.log('not solved for non-mutual connection between tiles', tileIndex, neighbour)
-                        startCheckAtIndex = tileIndex
-                        return false
-                    }
-                    if (neighbour!==fromIndex) {
-                        if (checked.has(neighbour)) {
-                            // it's a loop
-                            // console.log('not solved because of loop detected at tile', tileIndex)
-                            startCheckAtIndex = tileIndex
-                            return false
-                        } else {
-                            newChecks.add({fromIndex: tileIndex, tileIndex: neighbour})
-                        }
-                    }
-                }
-                checked.add(tileIndex)
-                toCheck = newChecks
-            }
+
+        // // console.log('=================== Solved check ======================')
+        // let toCheck = new Set([{fromIndex: -1, tileIndex: startCheckAtIndex}])
+        // console.log('start at', startCheckAtIndex)
+        // const checked = new Set([])
+        // while (toCheck.size > 0) {
+        //     // console.log('toCheck = ', toCheck)
+        //     const newChecks = new Set([])
+        //     for (let{fromIndex, tileIndex} of toCheck) {
+        //         // console.log('checking tile', tileIndex, 'coming from', fromIndex)
+        //         const neighbours = connections.get(tileIndex)
+        //         // console.log('tile neighbours', neighbours)
+        //         for (let neighbour of neighbours) {
+        //             // console.log('checking neighbour', neighbour)
+        //             if (neighbour===-1) {
+        //                 // not solved if any tiles point outside
+        //                 // console.log('not solved for outside connection in tile', tileIndex)
+        //                 startCheckAtIndex = tileIndex
+        //                 return false
+        //             }
+        //             const neighbourConnections = connections.get(neighbour)
+        //             // console.log('neighbour connections', neighbourConnections)
+        //             if (!neighbourConnections.has(tileIndex)) {
+        //                 // not solved if a connection is not mutual
+        //                 // console.log('not solved for non-mutual connection between tiles', tileIndex, neighbour)
+        //                 startCheckAtIndex = tileIndex
+        //                 return false
+        //             }
+        //             if (neighbour!==fromIndex) {
+        //                 if (checked.has(neighbour)) {
+        //                     // it's a loop
+        //                     // console.log('not solved because of loop detected at tile', tileIndex)
+        //                     startCheckAtIndex = tileIndex
+        //                     return false
+        //                 } else {
+        //                     newChecks.add({fromIndex: tileIndex, tileIndex: neighbour})
+        //                 }
+        //             }
+        //         }
+        //         checked.add(tileIndex)
+        //         toCheck = newChecks
+        //     }
+        // }
+        // if (checked.size < grid.total) {
+        //     // console.log('not solved because only', checked.size, 'of', grid.total, 'were reached')
+        //     // it's an island
+        //     return false
+        // }
+        if (components.size === 1) {
+            // there might be loops, so not a sure check
+            // works for now I guess?
+            // not deleting old code just in case
+            dispatch('solved')
+            return true
         }
-        if (checked.size < grid.total) {
-            // console.log('not solved because only', checked.size, 'of', grid.total, 'were reached')
-            // it's an island
-            return false
-        }
-        dispatch('solved')
-        return true
+        return false
     }
 
-    onMount(()=>{
+    function initializeBoard() {
+        // create components and fill in connections data
+        tiles.forEach((tile, index) => {
+            let directions = grid.getDirections(tile)
+            connections.set(
+                index, 
+                new Set(directions.map(direction => {
+                return grid.find_neighbour(index, direction)
+            })))
+            connections.get(index).delete(-1)
+
+            const component = {
+                id: componentCounter,
+                color: 'white',
+                tiles: new Set([index])
+            }
+            tileComponents.set(index, component.id)
+            components.set(component.id, component)
+            componentCounter += 1
+        })
+        // merge initial components of connected tiles
         tiles.forEach((tile, index) => {
             let directions = grid.getDirections(tile)
             handleConnections({detail: {
@@ -115,7 +267,13 @@
                 tileIndex: index,
             }})
         })
+        components.delete(-1)
+        initialized = true
+    }
+    onMount(()=>{
+        initializeBoard()
     })
+    // $: console.log(components, tileComponents)
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight />
@@ -127,7 +285,8 @@
         viewBox="-0.6 {0.5*YSTEP} {width + 1.0} {height*YSTEP}"
         >
         {#each tiles as tile, i (i)}
-            <Tile {tile} {i} {grid} {solved} fillColor={solved ? '#7DF9FF' : 'white'}
+            <Tile {tile} {i} {grid} {solved} 
+                fillColor={solved ? '#7DF9FF' : components.get(tileComponents.get(i)||-1).color}
                 on:connections={handleConnections}/>
         {/each}
     </svg>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,4 +6,9 @@
 
 <p> Only the <a href="/hexagonal/5/1"> hexagonal </a> variety is available for now.</p>
 
+<h2> Changelog </h2>
+<ul>
+<li><em>2022-06-27</em> Added flow visualisation with random colors</li>
+<li><em>2022-06-25</em> Initial release of hexagonal pipes puzzles</li>
+</ul>
 </div>
