b5df94c9226fda4dbd6bb466e218c35930f8a9f1
Daria Vasyukova <daria.voznyak@gmail.com>
2022-08-28 15:52:29 +0300

    Update changelog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -118,6 +118,7 @@
 <div class="container">
 	<h2>Changelog</h2>
 	<ul>
+		<li><em>2022-08-28</em> Zoom level now stays the same when you click "Next puzzle".</li>
 		<li>
 			<em>2022-08-27</em> Some changes for daily puzzles:
 			<ul>
