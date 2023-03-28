4376a6f4f7082b430f5418e76ca3ed5ff9c5c710
Daria Vasyukova <daria.voznyak@gmail.com>
2023-03-28 20:17:43 +0300

    Move rotation tests to polygon

--- src/lib/puzzle/grids/polygonutils.test.js
+++ src/lib/puzzle/grids/polygonutils.test.js
@@ -105,6 +105,74 @@ describe('Test octagrid square tile rotations', () => {
 	});
 });
 
+describe('Test tile rotations', () => {
+	const hexagon = new RegularPolygonTile(6, 0, 0.5);
+
+	it('Rotate an end tile counter-clockwise', () => {
+		let rotated = 1;
+		rotated = hexagon.rotate(rotated, -1);
+		expect(rotated).toBe(2);
+		rotated = hexagon.rotate(rotated, -1);
+		expect(rotated).toBe(4);
+		rotated = hexagon.rotate(rotated, -1);
+		expect(rotated).toBe(8);
+		rotated = hexagon.rotate(rotated, -1);
+		expect(rotated).toBe(16);
+		rotated = hexagon.rotate(rotated, -1);
+		expect(rotated).toBe(32);
+		rotated = hexagon.rotate(rotated, -1);
+		expect(rotated).toBe(1);
+	});
+
+	it('Rotate an end tile clockwise', () => {
+		let rotated = 1;
+		rotated = hexagon.rotate(rotated, 1);
+		expect(rotated).toBe(32);
+		rotated = hexagon.rotate(rotated, 1);
+		expect(rotated).toBe(16);
+		rotated = hexagon.rotate(rotated, 1);
+		expect(rotated).toBe(8);
+		rotated = hexagon.rotate(rotated, 1);
+		expect(rotated).toBe(4);
+		rotated = hexagon.rotate(rotated, 1);
+		expect(rotated).toBe(2);
+		rotated = hexagon.rotate(rotated, 1);
+		expect(rotated).toBe(1);
+	});
+
+	it('Rotate an end tile in multiples', () => {
+		let rotated = 1;
+		expect(hexagon.rotate(rotated, 1)).toBe(32);
+		expect(hexagon.rotate(rotated, 2)).toBe(16);
+		expect(hexagon.rotate(rotated, 3)).toBe(8);
+		expect(hexagon.rotate(rotated, 4)).toBe(4);
+		expect(hexagon.rotate(rotated, 5)).toBe(2);
+		expect(hexagon.rotate(rotated, 6)).toBe(1);
+		expect(hexagon.rotate(rotated, -5)).toBe(32);
+		expect(hexagon.rotate(rotated, -4)).toBe(16);
+		expect(hexagon.rotate(rotated, -3)).toBe(8);
+		expect(hexagon.rotate(rotated, -2)).toBe(4);
+		expect(hexagon.rotate(rotated, -1)).toBe(2);
+		expect(hexagon.rotate(rotated, -6)).toBe(1);
+	});
+
+	it('Rotate a straight tile in multiples', () => {
+		let rotated = 9;
+		expect(hexagon.rotate(rotated, 1)).toBe(36);
+		expect(hexagon.rotate(rotated, 2)).toBe(18);
+		expect(hexagon.rotate(rotated, 3)).toBe(9);
+		expect(hexagon.rotate(rotated, 4)).toBe(36);
+		expect(hexagon.rotate(rotated, 5)).toBe(18);
+		expect(hexagon.rotate(rotated, 6)).toBe(9);
+		expect(hexagon.rotate(rotated, -5)).toBe(36);
+		expect(hexagon.rotate(rotated, -4)).toBe(18);
+		expect(hexagon.rotate(rotated, -3)).toBe(9);
+		expect(hexagon.rotate(rotated, -2)).toBe(36);
+		expect(hexagon.rotate(rotated, -1)).toBe(18);
+		expect(hexagon.rotate(rotated, -6)).toBe(9);
+	});
+});
+
 describe('Test square tile get directions', () => {
 	const square = new RegularPolygonTile(4, 0, 0.5);
 
