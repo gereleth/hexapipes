fe150da848e3dbebd4ca7ced23e9cb841d7dbe1d
Daria Vasyukova <daria.voznyak@gmail.com>
2024-01-17 00:10:58 +0300

    Highlight play item on snubsquare page

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -23,6 +23,7 @@
 					$page.url.pathname.startsWith('/etrat') ||
 					$page.url.pathname.startsWith('/cube') ||
 					$page.url.pathname.startsWith('/trihexagonal') ||
+					$page.url.pathname.startsWith('/snubsquare') ||
 					$page.url.pathname.startsWith('/custom') ||
 					$page.url.pathname.startsWith('/play')}
 			>
