c2c3b15d614e45dc358081cb0c7d864428f36123
Daria Vasyukova <daria.voznyak@gmail.com>
2025-05-13 21:43:44 +0300

    update grid url param matcher

--- src/params/gridkind.js
+++ src/params/gridkind.js
@@ -1,6 +1,17 @@
+const pattern = new RegExp(
+	'^hexagonal$|^hexagonal-wrap$|' +
+		'^square$|^square-wrap$|' +
+		'^octagonal$|^octagonal-wrap$|' +
+		'^etrat$|^etrat-wrap$|' +
+		'^cube$|^cube-wrap$|' +
+		'^octagonal$|^octagonal-wrap$|' +
+		'^trihexagonal$|^trihexagonal-wrap$|' +
+		'^snubsquare$|^snubsquare-wrap$|' +
+		'^rhombitrihexagonal$|^rhombitrihexagonal-wrap$|' +
+		'^triangular$|^triangular-wrap$|'
+);
+
 /** @type {import('@sveltejs/kit').ParamMatcher} */
 export function match(param) {
-	return /hexagonal|hexagonal-wrap|square|square-wrap|octagonal|octagonal-wrap|etrat|etrat-wrap|cube|cube-wrap/.test(
-		param
-	);
+	return pattern.test(param);
 }
