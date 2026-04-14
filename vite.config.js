62b1328ab1e7b8a3d1de8874eead7cb908e805f2
najaad <46876019+najaad@users.noreply.github.com>
2026-04-13 21:48:13 -0400

    Add tailwinds plugin and imports in config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 // vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [tailwindcss(), sveltekit()],
 	test: {
 		globals: true,
 		environment: 'jsdom'
